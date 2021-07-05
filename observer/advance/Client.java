364c2f22a1a1e038161ef536494d3647e691f3d4
phannguyenlong <phannguyenlong0812@gmail.com>
2021-07-05 14:21:22 +0700

    observer: add comment

--- observer/advance/Client.java
+++ observer/advance/Client.java
@@ -4,6 +4,11 @@ import observer.advance.observer.RecieveObserver;
 import observer.advance.observer.UpperObserver;
 import observer.advance.observer.CountObserver;
 
+/**
+ * This is more advance cause Observer can choose when it ready to be attached to Service
+ * => Client dont need to add Observer to the Service
+ * Client only need to create the Observer and it will be automaticaly add to Service when it is ready
+ */
 public class Client {
     public static void main(String[] args) {
         // Creating the service
